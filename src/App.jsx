5bb5933d45446114aa107f526a597c20287530a9
Vincent <Vincent.rey865@icloud.com>
2023-11-19 22:46:58 +0100

    todo app upload "handlePatch"

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   const [todoList, setTodoList] = useState([]);
   const [titulo, setTitulo] = useState("");
   const [fecha, setFecha] = useState("");
+  const [editingText, setEditingText] = useState("");
+  const [editingDate, setEditingDate] = useState("");
+  const [isEditing, setIsEditing] = useState(null);
 
   const handlePostClick = () => {
     fetch("http://localhost:3000/todo", {
@@ -48,34 +51,71 @@ function App() {
         console.error("Error:", error);
       });
   };
+  const handleToggleDone = async (id, newDoneValue) => {
+    const todoToUpdate = todoList.find((todo) => todo.id === id);
 
-  const handlePatch = (id) => {
-    const updatedTodoList = todoList.map((todo) =>
-      todo.id === id ? { ...todo, done: !todo.done } : todo
-    );
-
-    const todoToUpdate = updatedTodoList.find((todo) => todo.id === id);
-
-    fetch(`http://localhost:3000/todo/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text: todoToUpdate.text,
-        fecha: todoToUpdate.fecha,
-        done: todoToUpdate.done,
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("La respuesta de red no fue correcta.");
-        }
-        setTodoList(updatedTodoList);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
+    try {
+      const response = await fetch(`http://localhost:3000/todo/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: todoToUpdate.text,
+          fecha: todoToUpdate.fecha,
+          done: newDoneValue,
+        }),
       });
+
+      if (!response.ok) {
+        throw new Error("La respuesta de red no fue correcta.");
+      }
+
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, done: newDoneValue } : todo
+      );
+
+      setTodoList(updatedTodoList);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handlePatch = async (id) => {
+    const todoToUpdate = todoList.find((todo) => todo.id === id);
+
+    try {
+      const response = await fetch(`http://localhost:3000/todo/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: isEditing === id ? editingText : todoToUpdate.text,
+          fecha: isEditing === id ? editingDate : todoToUpdate.fecha,
+          done: todoToUpdate.done,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("La respuesta de red no fue correcta.");
+      }
+
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              text: isEditing === id ? editingText : todo.text,
+              fecha: isEditing === id ? editingDate : todo.fecha,
+            }
+          : todo
+      );
+
+      setTodoList(updatedTodoList);
+      setIsEditing(null); // Termina la edición después de la actualización
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   useEffect(() => {
@@ -120,21 +160,44 @@ function App() {
             onChange={(e) => setFecha(e.target.value)}
           />
         </div>
-        <button onClick={handlePostClick}>Send</button>
+        <button onClick={handlePostClick}>Submit</button>
       </div>
       <div className="todo-list">
         {todoList.map((todo) => (
           <div className="card" key={todo.id}>
-            <div className={`headCard ${todo.done ? "done" : "undone"}`}>
-              <h2 className="cardTitle">{todo.text}</h2>
-              <input
-                type="checkbox"
-                checked={todo.done}
-                onChange={() => handlePatch(todo.id)}
-              />
-            </div>
-            <p className="cardDate">{todo.fecha}</p>
-            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            {isEditing === todo.id ? (
+              <div>
+                <input
+                  type="text"
+                  value={editingText}
+                  onChange={(e) => setEditingText(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editingDate}
+                  onChange={(e) => setEditingDate(e.target.value)}
+                />
+                <button onClick={() => handlePatch(todo.id)}>Save</button>
+              </div>
+            ) : (
+              <div>
+                <div className={`headCard ${todo.done ? "done" : "undone"}`}>
+                  <h2 className="cardTitle">{todo.text}</h2>
+                  <input
+                    type="checkbox"
+                    checked={todo.done}
+                    onChange={() => handleToggleDone(todo.id, !todo.done)}
+                  />
+                </div>
+                <div className="footCard">
+                  <p className="cardDate">{todo.fecha}</p>
+                  <button onClick={() => setIsEditing(todo.id)}>Modify</button>
+                </div>
+              </div>
+            )}
+            <button className="delete" onClick={() => handleDelete(todo.id)}>
+              Delete
+            </button>
           </div>
         ))}
       </div>
